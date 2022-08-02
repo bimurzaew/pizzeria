bcfe2e87e1035ee0f2cca5666af052462837bc61
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2022-08-02 12:05:35 +0300

    типизировал слайс пицц

--- src/redux/slices/pizzaSlice.ts
+++ src/redux/slices/pizzaSlice.ts
@@ -2,17 +2,6 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "../store";
 
-export const fetchPizzas = createAsyncThunk(
-  "pizza/fetchPizzas",
-  async (params) => {
-    const { category, sortBy, order, searched, pageCount } = params;
-    const { data } = await axios.get(
-      `https://62dd52efccdf9f7ec2c4e0b8.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${searched}&page=${pageCount}&limit=4`
-    );
-    return data;
-  }
-);
-
 type PizzaItem = {
   category: number;
   description: string;
@@ -34,23 +23,34 @@ const initialState: PizzaSliceState = {
   status: "loading",
 };
 
+export const fetchPizzas = createAsyncThunk<
+  PizzaItem[],
+  Record<string, string>
+>("pizza/fetchPizzas", async (params) => {
+  const { category, sortBy, order, searched, pageCount } = params;
+  const { data } = await axios.get<PizzaItem[]>(
+    `https://62dd52efccdf9f7ec2c4e0b8.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${searched}&page=${pageCount}&limit=4`
+  );
+  return data;
+});
+
 const pizzaSlice = createSlice({
   name: "pizza",
   initialState,
   reducers: {},
-  extraReducers: {
-    [fetchPizzas.pending]: (state) => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchPizzas.pending, (state) => {
       state.status = "loading";
       state.items = [];
-    },
-    [fetchPizzas.fulfilled]: (state, action) => {
+    });
+    builder.addCase(fetchPizzas.fulfilled, (state, action) => {
       state.status = "success";
       state.items = action.payload;
-    },
-    [fetchPizzas.rejected]: (state) => {
+    });
+    builder.addCase(fetchPizzas.rejected, (state) => {
       state.status = "error";
       state.items = [];
-    },
+    });
   },
 });
 
